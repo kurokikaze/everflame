bdf80c1beb953fc6eaae011af9cfa9456c94e784
Shirokov <sergei.shirokov@t-systems.com>
2024-10-24 19:05:05 +0200

    Keys generated and hidden, JWT type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,24 @@
-type Challenge = {
+export type Challenge = {
     userId: number
     comment: string
     dateCreated: Date
 }
+
+export interface ISingleJwt {
+    secret: string;
+    time: number;
+  }
+  
+  export interface IAccessJwt {
+    publicKey: string;
+    privateKey: string;
+    time: number;
+  }
+  
+  export interface IJwt {
+    access: IAccessJwt;
+    confirmation: ISingleJwt;
+    resetPassword: ISingleJwt;
+    refresh: ISingleJwt;
+  }
+  
